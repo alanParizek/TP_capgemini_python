f3e10dda5ba7ed3d83835d0639f8b88c63080a2e
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2022-11-16 17:05:17 -0300

    agregando static

--- listaDeCompras/listaDeCompras/settings.py
+++ listaDeCompras/listaDeCompras/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, 'listaDeCompras/static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
