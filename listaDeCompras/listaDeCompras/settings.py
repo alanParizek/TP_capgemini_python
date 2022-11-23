fe875d84c551b9c40b22f8c2bbd2e87459f99e88
Santiagomorel <smorel@frba.utn.edu.ar>
2022-11-23 16:32:10 -0300

    estructura inicial del login

--- listaDeCompras/listaDeCompras/settings.py
+++ listaDeCompras/listaDeCompras/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'listaDeCompras/static'),
+   os.path.join(BASE_DIR, 'usuarios/static'),
 )
 
 # Default primary key field type
