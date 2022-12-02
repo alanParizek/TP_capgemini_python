3603ee18f6f07a4c19eede72f0cec1294e1943b6
Marco Rodriguez Melgarejo <mrodrguezmelgarejo@frba.utn.edu.ar>
2022-12-02 15:53:35 -0300

    eliminamos la clase usuario

--- listaDeCompras/usuarios/forms.py
+++ listaDeCompras/usuarios/forms.py
@@ -1,13 +1,14 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import Usuario
+from django.conf import settings
+from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     class Meta:
-        model = Usuario
+        model = User
         fields = ['username', 'first_name', 'last_name', 'email']
 
 class LogInForm(ModelForm):
     class Meta:
-        model = Usuario
+        model = User
         fields = ['username', 'password']
