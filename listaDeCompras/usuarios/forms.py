44b13190406f3453c1984aad155038770c5d9555
Santiagomorel <smorel@frba.utn.edu.ar>
2022-12-04 01:26:46 -0300

    pruebas

--- listaDeCompras/usuarios/forms.py
+++ listaDeCompras/usuarios/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 class LogInForm(ModelForm):
     class Meta:
