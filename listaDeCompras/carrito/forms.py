81e88ea0b86756daaaabd1ba56d06bd7b62fef86
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2022-12-05 16:28:59 -0300

    falta agregar el VDI y hacer pruebas con el template

--- listaDeCompras/carrito/forms.py
+++ listaDeCompras/carrito/forms.py
@@ -5,17 +5,17 @@ class AgregarProductoForm(ModelForm):
     # valor inicial para el ModelChoiceField (buscar la respuesta que habla de eso):
     # https://stackoverflow.com/questions/657607/setting-the-selected-value-on-a-django-forms-choicefield
 
+    def __init__(self, *args, **kwargs):
+        self.fields['producto'].queryset = Producto.objects.all()
+
     @classmethod
     def formularioConValoresIniciales(cls, valoresIniciales: tuple[Producto, int] ):
         form = AgregarProductoForm()
         producto, cantidad = valoresIniciales
-        form.fields['producto'].initial = producto
+        form.fields['producto'].initial = producto #si no anda fijarse con form.initial['producto'] = producto
         form.fields['cantidad'].initial = cantidad
         return form
 
     class Meta:
         model = ChangoXproducto
-        fields = [producto=models.ModelChoiceField(queryset=Producto.objects.all()), cantidad=models.IntegerField()]
-        
-class FormFoto(ModelForm):
-    pass
+        fields = ['producto', 'cantidad']
--- listaDeCompras/carrito/views.py
+++ listaDeCompras/carrito/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Chango, ChangoXproducto
-from .forms import AgregarProductoForm, FormFoto
+from .forms import AgregarProductoForm
 
 
 # vamos a tener fomulario foto y formulario carrito
@@ -17,38 +18,51 @@ class ChangoController():
     @staticmethod
     @login_required()
     def verCarrito(request):
-        formAgregarProducto = AgregarProductoForm.formularioConValoresIniciales(request.session['valoresIniciales'])
-        formFoto = FormFoto() # hacer FormFoto
+        try:
+            formAgregarProducto = AgregarProductoForm.formularioConValoresIniciales(request.session['valoresIniciales'])
+        except:
+            formAgregarProducto = AgregarProductoForm()
+        chango = Chango.carritoDelUsuario(request.user)
+        itemsCarrito = ChangoXproducto.objects.filter(chango=chango)
         # agregar al context los productos en el carrito del usuario logueado para que los muestre (obtenemos el usuario de la session)
-        context = {'formSubirFoto': formFoto, 'formAgregarProducto':formAgregarProducto}
-        return render(request, "carrito.html") #, context)
+        context = {'formAgregarProducto':formAgregarProducto, 'itemsCarrito':itemsCarrito}
+        return render(request, "carrito.html", context)
 
     @staticmethod
     @login_required()
     def chequearFoto(request):
         # agarra la foto, llama al VDI y mete en la session los valores de producto y cantidad
-        request.session['valoresIniciales'] = # agregarle la tupla
-        return render(request, "carrito.html")
+        request.session['valoresIniciales'] = (producto, cantidad)# agregarle la tupla con el VDI
+        ChangoController.verCarrito(request)
 
     @staticmethod
     @login_required()
     def agregarProducto(request):
         chango = Chango.carritoDelUsuario(request.user)
-        form = AgregarProductoForm(request.POST, request.session['productoAagregar'])
+        form = AgregarProductoForm(request.POST)
         if form.is_valid():
             chango.agregarProducto(request.POST['producto'], request.POST['cantidad'])
-        
+        ChangoController.eliminarDefaultsSession()
         # agregar el producto al carrito de ese usuario (obtenemos el usuario de la session)
         # redireccionar a la pagina de ver carrito
-        return render(request, "carrito.html")
+        ChangoController.verCarrito(request)
 
     @staticmethod
     @login_required()
     def sacarProducto(request):
         chango = Chango.carritoDelUsuario(request.user)
         chango.sacarTodosDeUnProducto(request.POST['producto'])
+        ChangoController.eliminarDefaultsSession()
         # redireccionar a la pagina de ver carrito
-        return render(request, "carrito.html")
+        ChangoController.verCarrito(request)
+        
+    @staticmethod
+    def eliminarDefaultsSession(request):
+        try:
+            request.session.pop('valoresIniciales')
+        except:
+            pass
+        # borra la key del dict de la session
 
     @staticmethod
     @login_required()
@@ -62,7 +76,7 @@ class ChangoController():
         # crearle un carrito nuevo
         # hacer otras cosas?
         # redireccionar a la pagina de ver carrito
-        return render(request, "carrito.html")
+        return HttpResponse('Gracias por su compra, sera atendido por nuestros cajeros en un instante!')
 
     
     
