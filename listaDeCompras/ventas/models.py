43f3d7d3002124ff83828595996c69ef91b92465
Santiagomorel <smorel@frba.utn.edu.ar>
2022-12-07 17:56:53 -0300

    corregido css del carrito

--- listaDeCompras/ventas/models.py
+++ listaDeCompras/ventas/models.py
@@ -4,11 +4,5 @@ from datetime import date
 
 # Create your models here.
 
-
-class Venta(models.Model):
-    usuario = 
-    jsonFile = 
-    
-    @classmethod
-    def registrarVenta(carrito):
-        # arma la venta con el carrito y la guarda en la DB
+def Venta(request):
+    return 
